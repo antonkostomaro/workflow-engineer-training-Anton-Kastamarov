b897c61c5aa0f92a421794381c037cb81e4118bc
antonkostomaro <antonanton102@mail.ru>
2021-03-15 22:02:55 +0300

    add Exercise2.2.py

---
+++ Exercise1.9.py
@@ -0,0 +1,35 @@
+"""
+Exercise 2.1.
+42 = n--не допустимо так как это число с зарезервированным id,и  по договору нельзя
+к числу присваивать переменную
+x = y = 1 --тут создается две колонки с присвоением 1-це к переменной x=y, y=1 значит
+x,y будут приходить к одной колонке
+In some languages every statement ends with a semi-colon, ;. What happens if you put a
+semi-colon at the end of a Python statement?:
+в python ';' не используется если же так сделать то тогда будет ошибка 'SyntaxError'
+
+What if you put a period at the end of a statement?:
+будет так же "SyntaxError"
+
+In math notation you can multiply x and y like this: xy. What happens if you try that in
+Python?
+будет ошибка "SyntaxError" так как умножение в python делается через знак *
+
+
+Exercise 2.2.
+The volume of a sphere with radius r is 4
+3
+?r
+3
+. What is the volume of a sphere with radius 5?
+523.59877559829886
+
+Suppose the cover price of a book is $24.95, but bookstores get a 40% discount. Shipping costs
+$3 for the first copy and 75 cents for each additional copy. What is the total wholesale cost for
+60 copies?
+1010.7
+If I leave my house at 6:52 am and run 1 mile at an easy pace (8:15 per mile), then 3 miles at
+tempo (7:12 per mile) and 1 mile at easy pace again, what time do I get home for breakfast?
+
+7:30:06
+"""
