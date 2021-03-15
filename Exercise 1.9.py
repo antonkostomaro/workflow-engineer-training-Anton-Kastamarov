41f0bfe79cea99c6cb450a5ff26498e1a3d531f3
antonkostomaro <antonanton102@mail.ru>
2021-03-15 21:07:21 +0300

    Exercise 1.9

---
+++ Exercise 1.9.py
@@ -0,0 +1,36 @@
+
+"""print('helloword) если не записать скобку то программа будет
+считать что функция  не закончена , и напишет о ошибке SyntaxError
+print('helloword') если вы хотите правильно написать эту функцию тогда нужно учесть
+правила написание этой функции т.к эта функция была построена с определенными параметрами
+для ее реализации
+"""
+
+"""
+1. In a print statement, what happens if you leave out one of the parentheses, or both?
+произойдет ошибка синтаксиса(SyntaxError) и вам напишут на какой строке и где нужно 
+поставить скобки
+2. If you are trying to print a string, what happens if you leave out one of the quotation marks,
+or both?
+Python не поймет этого и предложит исправить вам это 
+3. You can use a minus sign to make a negative number like -2. What happens if you put a plus
+sign before a number? What about 2++2?
+если поставить перед числом "+" Тогда число будет добавляться к другому числу либо просто
+выведет число 2
+что на счет "++"то это будет тоже самое сложение 
+с ведущими нулями в python сложнее так как не разрешается это кроме дробных чисел 
+е
+5. What happens if you have two values with no operator between them?
+произойдет SyntaxError
+"""
+#Exercise 1.2.
+"""
+1. How many seconds are there in 42 minutes 42 seconds?:2562
+2. How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.
+:6.172839506172839
+3. If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per
+mile in minutes and seconds)? What is your average speed in miles per hour?
+:6.9174--(время на
+милю в минутах и секундах)
+Какая у вас средняя скорость в милях в час?:0.11528999999999999
+"""
